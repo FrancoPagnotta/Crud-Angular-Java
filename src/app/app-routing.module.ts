24b9ce1323d0dff64d656d62b45cea8e41cae6c8
FrancoPagnotta <fpagnotta@mobydigital.com>
2022-02-14 14:31:45 -0300

    Implement routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ClientsComponent } from './clients/components/clients.component';
+
+const routes: Routes = [
+  { path: 'Clients', component: ClientsComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'Clients' }
+]
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ClientsModule } from './clients/clients.module';
@@ -11,8 +13,10 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    RouterModule,
     SharedModule,
-    ClientsModule
+    ClientsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
